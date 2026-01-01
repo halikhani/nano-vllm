deb66510301495a88e8702496c3c268326f4df5d
Hamidreza Alikhani <ha.alikhani.k@gmail.com>
2026-01-01 14:53:57 -0800

    init submodules for qwen3 finished, back to layers/ to understand nn.Module blocks

--- nanovllm/models/qwen3.py
+++ nanovllm/models/qwen3.py
@@ -3,4 +3,83 @@ from torch import nn
 import torch.distributed as dist
 from transformers import Qwen3Config
 
-from nanovllm.layers import SiluAndMul
+from nanovllm.layers.activation import SiluAndMul
+from nanovllm.layers.attention import Attention
+from nanovllm.layers.layernorm import RMSNorm
+from nanovllm.layers.linear import QKVParallelLinear, MergedColumnParallelLinear, RowParallelLinear
+from nanovllm.layers.rotary_embedding import get_rope
+from nanovllm.layers.embed_head import VocabParallelEmbedding, ParallelLMHead
+
+
+class Qwen3Attention(nn.Module):
+
+    def __init__(
+        self,
+        hidden_size: int,
+        num_heads: int, # total query heads
+        num_kv_heads: int, # total key/value heads (used for multi-query attention)
+        max_position: int = 4096 *32,
+        head_dim: int | None = None,
+        rms_norm_eps: float = 1e-06,
+        qkv_bias: bool = False,
+        rope_theta: float = 10000, # base frequency
+        rope_scaling: tuple | None = None, # scaling for longer sequences
+    ) -> None:
+        super().__init__()
+        tp_size = dist.get_world_size()
+        self.total_num_heads = num_heads
+        assert self.total_num_heads % tp_size == 0
+        self.num_heads = self.total_num_heads // tp_size
+        self.total_num_kv_heads = num_kv_heads 
+        assert self.total_num_kv_heads % tp_size == 0
+        self.num_kv_heads = self.total_num_kv_heads // tp_size
+        self.head_dim = head_dim or hidden_size // self.total_num_heads
+        self.q_size = self.num_heads * self.head_dim
+        self.kv_size = self.num_kv_heads * self.head_dim
+        self.scaling = self.head_dim** -0.5
+        self.qkv_bias = qkv_bias
+
+
+        # Column-parallel QKV projection (split across GPUs)
+        # Weight dimensions (tp_size=2):
+        # Full weight: [896, (14 + 2 + 2) * 64] = [896, 1152]
+        # Q: 14 * 64 = 896
+        # K: 2 * 64 = 128
+        # V: 2 * 64 = 128
+        # Per GPU: [896, 576] (column-parallel, split along output)
+        # Q: 7 * 64 = 448
+        # K: 1 * 64 = 64
+        # V: 1 * 64 = 64
+        self.kqv_proj = QKVParallelLinear(
+            hidden_size,
+            self.head_dim,
+            self.total_num_heads,
+            self.total_num_kv_heads,
+            bias=self.qkv_bias,
+        )
+
+        # Column-parallel QKV projection (split across GPUs)
+        self.o_proj = RowParallelLinear(
+            self.total_num_heads * self.head_dim,
+            hidden_size,
+            bias=False,
+        )
+        self.rotary_emb = get_rope(
+            self.head_dim,
+            rotary_dim=self.head_dim,
+            max_position=max_position,
+            base=rope_theta,
+            rope_scaling=rope_scaling,
+        )
+        self.attn = Attention(
+            self.num_heads,
+            self.head_dim,
+            self.scaling,
+            self.num_kv_heads,
+        )
+        if not self.qkv_bias:
+            self.q_norm = RMSNorm(self.head_dim, eps=rms_norm_eps)
+            self.k_norm = RMSNorm(self.head_dim, eps=rms_norm_eps)
+    
+    def forward():
+        pass
